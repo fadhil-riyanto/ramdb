0e77447bd057666d40bc1459630778846c0830bf
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2022-10-09 13:28:11 +0700

    use 2 dimension array for save timstamp

--- ramdb/ramdb.cs
+++ ramdb/ramdb.cs
@@ -1,15 +1,17 @@
 ﻿using ramdb.src;
 using Newtonsoft.Json;
 using ramdb.Except;
+using System;
 
 
 namespace ramdb;
 public class Ramdb
 {
-        private Dictionary<string, string>[] mem;
+        private Dictionary<string, object[]>[] mem;
         private int dbnum;
         private int max_db = 100;
         private IOHelper ih;
+        private string tempkey;
 
         public Ramdb(string filename, string folder)
         {
@@ -25,10 +27,10 @@ public class Ramdb
                 }
                 catch (System.ArgumentNullException) // if file is blank / new file
                 {
-                        this.mem = new Dictionary<string, string>[max_db];
+                        this.mem = new Dictionary<string, object[]>[max_db];
                         for (int a = 0; a < max_db; a++)
                         {
-                                this.mem[a] = new Dictionary<string, string>();
+                                this.mem[a] = new Dictionary<string, object[]>();
                         }
                 }
         }
@@ -37,9 +39,9 @@ public class Ramdb
 
 
         }
-        private Dictionary<string, string>[] loadfromfile()
+        private Dictionary<string, object[]>[] loadfromfile()
         {
-                return JsonConvert.DeserializeObject<Dictionary<string, string>[]>(this.ih.ReadFile());
+                return JsonConvert.DeserializeObject<Dictionary<string, object[]>[]>(this.ih.ReadFile());
         }
         public Ramdb setDb(int db)
         {
@@ -50,21 +52,42 @@ public class Ramdb
                 this.dbnum = db;
                 return this;
         }
-        public void set(string key, string value)
+        public Ramdb set(string key, string value)
         {
+                this.tempkey = key;
                 try
                 {
-                        volatile_mem.set(this.mem[dbnum], key, value);
+                        object[] data = {
+                                value, (long)0
+                        };
+                        volatile_mem.set(this.mem[dbnum], key, data);
                 }
                 catch (System.ArgumentException)
                 {
-                        this.mem[dbnum][key] = value;
+                        this.mem[dbnum][key][0] = value;
                 }
+                return this;
+
+        }
+        public void DeleteAfter(long? second)
+        {
+                if (second != 0 && second != null)
+                {
+                        DateTime foo = DateTime.Now;
+                        long unixTime = ((DateTimeOffset)foo).ToUnixTimeSeconds();
+                        this.mem[dbnum][this.tempkey][1] = unixTime + second;
+                }
+
 
         }
         public string get(string key)
         {
+                middleware.deleteAutoAfter(this.mem, dbnum);
                 return volatile_mem.get(this.mem[dbnum], key);
+        }
+        public bool del(string key)
+        {
+                return volatile_mem.del(this.mem[dbnum], key);
         }
         public void commit()
         {
--- ramdb/src/volatile.cs
+++ ramdb/src/volatile.cs
@@ -3,14 +3,24 @@ namespace ramdb.src
 {
         class volatile_mem
         {
-                public static void set(Dictionary<string, string> mem, string keytoset, string valuetoset)
+                public static void set(Dictionary<string, object[]> mem, string keytoset, object[] data)
                 {
-                        mem.Add(keytoset, valuetoset);
+                        mem.Add(keytoset, data);
                 }
-                public static string get(Dictionary<string, string> mem, string keytoset)
+                public static string get(Dictionary<string, object[]> mem, string keytoget)
                 {
                         try{
-                                return mem[keytoset];
+                                return (string)mem[keytoget][0];
+                        } catch (System.Collections.Generic.KeyNotFoundException)
+                        {
+                                throw new ramdb.Except.KeyNotFoundException("key not found");
+                        }
+                        
+                }
+                public static bool del(Dictionary<string, object[]> mem, string keytodel)
+                {
+                        try{
+                                return mem.Remove(keytodel);
                         } catch (System.Collections.Generic.KeyNotFoundException)
                         {
                                 throw new ramdb.Except.KeyNotFoundException("key not found");
